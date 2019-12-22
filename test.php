d680b0c8c526e9306f2f4fe2bb21a49a75cb345e
David Easter <36341298+TheBloodEaglez@users.noreply.github.com>
2019-12-21 18:40:16 -0600

    Create test.php

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+function file_get_contents_curl($url) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return the data instead of printing it to the browser.
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
+
+$query = "puppy github";
+$url = 'http://www.google.co.in/search?q='.urlencode($query).'';
+$scrape = file_get_contents_curl($url);
+
+?>
